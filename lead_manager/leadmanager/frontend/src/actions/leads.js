43aba19df84d29f79d7a5d9fda53e57548c5344e
MartinMwiti <martinmwiti007@gmail.com>
2020-06-22 23:31:11 +0300

    react django connected

--- lead_manager/leadmanager/frontend/src/actions/leads.js
+++ lead_manager/leadmanager/frontend/src/actions/leads.js
@@ -6,4 +6,11 @@ import { GET_LEADS } from './types'
 // GET LEADS
 export const getLeads = () => dispatch =>{
     axios.get('api/leads/') // since i'm in the same server. no need to add the prefix 'localhost:8000/'
+        .then(res => {
+            dispatch({
+                type: GET_LEADS,
+                payload: res.data
+            })
+            .catch(err=console.log(err))
+        })
 }
--- lead_manager/leadmanager/frontend/src/components/leads/Leads.js
+++ lead_manager/leadmanager/frontend/src/components/leads/Leads.js
@@ -1,13 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+
+import { getLeads } from  '../../actions/leads'
 
 class Leads extends Component {
+    static propTypes = {
+        leads: PropTypes.array.isRequired
+    }
+
+    componentDidMount(){
+        this.props.getLeads()
+    } // call the action
+    
     render() {
         return (
-            <div>
-                <h1>Leads List</h1>
-            </div>
-        )
+          <Fragment>
+            <h2>Leads List</h2>
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Message</th>
+                  <th />
+                </tr>
+              </thead>
+              <tbody>
+                {this.props.leads.map((lead) => (
+                  <tr key={lead.id}>
+                    <td>{lead.id}</td>
+                    <td>{lead.name}</td>
+                    <td>{lead.email}</td>
+                    <td>{lead.message}</td>
+                    <td>
+                      <button className="btn btn-danger btn-sm">Delete</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </Fragment>
+        );
     }
 }
 
-export default Leads;
+const mapStateToProps = state => ({
+    leads: state.leads.leads
+    // 'leads': refers to the props i'll use above. 'state.leads' refers to the leads reducer. The lasts 'leads' refers to the state found in the leads reducer
+})
+
+export default connect(mapStateToProps, {getLeads})(Leads);
