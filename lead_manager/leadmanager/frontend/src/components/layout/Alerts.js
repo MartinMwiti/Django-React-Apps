edcba9c2027713e6bdb2709fcb15ba9580579fda
MartinMwiti <martinmwiti007@gmail.com>
2020-06-23 21:03:05 +0300

    error handling update

--- lead_manager/leadmanager/frontend/src/components/layout/Alerts.js
+++ lead_manager/leadmanager/frontend/src/components/layout/Alerts.js
@@ -1,15 +1,30 @@
 import React, { Component, Fragment } from "react";
 import { withAlert } from "react-alert";
-
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 class Alerts extends Component {
-    componentDidMount() {
-        this.props.alert.show("this is working")
-    }
+  static propTypes = {
+    error: PropTypes.object.isRequired,
+  };
 
-    render() {
-        return <Fragment />        
+  componentDidUpdate(prevProps) {
+    const { error, alert } = this.props;
+    if (error !== prevProps.error) {
+        //if(error.msg.name) alert.error('Name is required')
+        error.msg.name && alert.error(`Name: ${error.msg.name.join()}`); // shorter but same as above code.
+        error.msg.email && alert.error(`Email: ${error.msg.email.join()}`); //'`Name: ${error.msg.join()}` since the error is an array so i use join() to make it a string
+      
     }
-    
+  }
+
+  render() {
+    return <Fragment />;
+  }
 }
-export default withAlert()(Alerts);
+
+const mapStateToProps = (state) => ({
+  error: state.errors, // will get the 'msg' and 'status'
+});
+
+export default connect(mapStateToProps)(withAlert()(Alerts));
