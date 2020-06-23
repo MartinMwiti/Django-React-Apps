e8ff998ec23445cad2fc20b03b35e359ee2d61cf
MartinMwiti <martinmwiti007@gmail.com>
2020-06-23 12:35:03 +0300

    redux & http request

--- lead_manager/leadmanager/frontend/src/actions/leads.js
+++ lead_manager/leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,21 @@
 import axios from 'axios'
 
-import { GET_LEADS, DELETE_LEAD } from './types'
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
+
+
+// ADD LEAD
+export const addLead = (lead) => (dispatch) => {
+  axios
+    .post("/api/leads/", lead) // since i'm in the same server. no need to add the prefix 'localhost:8000/'
+    .then((res) => {
+      dispatch({
+        type: ADD_LEAD,
+        payload: res.data,
+      }).catch((err = console.log(err)));
+    });
+};
+
+
 
 
 // GET LEADS
@@ -27,3 +42,4 @@ export const deleteLead = (id) => (dispatch) => {
     })
     .catch((err) => console.log(err))
 };
+
--- lead_manager/leadmanager/frontend/src/reducers/leads.js
+++ lead_manager/leadmanager/frontend/src/reducers/leads.js
@@ -1,4 +1,4 @@
-import { GET_LEADS, DELETE_LEAD } from '../actions/types'
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "../actions/types";
 
 const initialState = {
     leads: []
@@ -6,6 +6,12 @@ const initialState = {
 
 export default function(state=initialState, action){
     switch (action.type) {
+      case ADD_LEAD:
+        return {
+          ...state,
+          leads: [...state.leads, action.payload], // take all leads already existing in the initialState and add the dispatched payload.
+        };
+
       case GET_LEADS:
         return {
           ...state,
