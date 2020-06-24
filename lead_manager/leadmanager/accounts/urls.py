dea02bb4b76df84a676121890556e041d9e2a888
MartinMwiti <martinmwiti007@gmail.com>
2020-06-24 19:30:14 +0300

    authentication update

--- lead_manager/leadmanager/accounts/urls.py
+++ lead_manager/leadmanager/accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('api/auth', include('knox.urls')),
     path('api/auth/register', RegisterAPI.as_view()),
     path('api/auth/login', LoginApi.as_view()),
-    path('api/auth/user', UserApi.as_view())
+    path('api/auth/user', UserApi.as_view()),
+    path('api/auth/logout', knox_views.LogoutView.as_view(), name="knox_logout") # invalidates/destroys a token. You need to login to get a new token.
 ]
