8c17ad8bc22ac9efa6b4f7d5db0c9228bf9d365c
MartinMwiti <martinmwiti007@gmail.com>
2020-06-24 14:02:27 +0300

    authentication setup

--- lead_manager/leadmanager/frontend/src/actions/leads.js
+++ lead_manager/leadmanager/frontend/src/actions/leads.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-import { createMessage } from  './messages'
+import { createMessage, returnErrors } from  './messages'
 
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 
 // ADD LEAD
@@ -16,16 +16,18 @@ export const addLead = (lead) => (dispatch) => {
         payload: res.data,
       });
     })
-    .catch(err => {
-      const errors = {
-        msg: err.response.data,
-        status: err.response.status,
-      };
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors, // bundled
-      });
-    });
+    .catch(err => 
+      dispatch(returnErrors(err.response.data, err.response.status))
+    //   const errors = {
+    //     msg: err.response.data,
+    //     status: err.response.status,
+    //   };
+    //   dispatch({
+    //     type: GET_ERRORS,
+    //     payload: errors, // bundled
+    //   });
+    // }
+    );
 };
 
 // GET LEADS
@@ -37,8 +39,10 @@ export const getLeads = () => (dispatch) => {
         type: GET_LEADS,
         payload: res.data,
       });
-    });
-  // .catch((err = console.log(err.response.data)));
+    })
+  .catch(err => 
+      dispatch(returnErrors(err.response.data, err.response.status)) // the error visible on the console.
+  )
 };
 
 // DELETE LEAD
--- lead_manager/leadmanager/leads/api.py
+++ lead_manager/leadmanager/leads/api.py
@@ -5,8 +5,18 @@ from rest_framework import viewsets, permissions
 
 # Lead ViewSet
 class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
+    # queryset = Lead.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
+
     serializer_class = LeadSerializer
+    # OVERRIDING
+    def get_queryset(self):
+        return self.request.user.leads.all() # get all leads related to a user
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    
+# 'perform_create' is called within the create method to call the serializer for creation once it's known the serialization is valid. Specifically, serializer.save()
