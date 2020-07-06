c551782ec71626d515ad4b47198c075231f19c41
MartinMwiti <martinmwiti007@gmail.com>
2020-07-06 17:24:44 +0300

    update

--- real_estate/backend/real_estate/urls.py
+++ real_estate/backend/real_estate/urls.py
@@ -6,5 +6,16 @@ from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('api/accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #urls for my media to be able to access via urls
+
+# catch all other routes not in the above urlpatterns. i.e the one used in react frontend and 404 errors
+urlpatterns += [re_path(r'^.*',
+                        TemplateView.as_view(template_name='index.html'))]
+
