c341db3d561ee9d65bd85831501ecab43de9fee0
MartinMwiti <martinmwiti007@gmail.com>
2020-07-09 15:19:50 +0300

    update

--- real_estate/frontend/src/App.js
+++ real_estate/frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+// component
+import Layout from './hocs/Layout'
 
-function App() {
+
+const App = ()=> {
   return (
-    <div >
-      App
-    </div>
+    <Layout> 
+      
+    </Layout>
   );
 }
 
