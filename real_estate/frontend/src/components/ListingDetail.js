227b859cca79e8b526e1f4cd73ac8b9548cc5713
MartinMwiti <martinmwiti007@gmail.com>
2020-07-09 15:05:05 +0300

    update

---
+++ real_estate/frontend/src/components/ListingDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ListingDetail() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ real_estate/frontend/src/hocs/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// components
+import Navbar from '../components/Navbar'
+
+const Layout = (props) => {
+    return (
+        <div>
+            <Navbar />
+            {props.children}
+        </div>
+    )
+}
+
+export default Layout
