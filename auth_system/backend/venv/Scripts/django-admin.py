25c62607533f4cdd13377aa3ce41f4da64c88487
MartinMwiti <martinmwiti007@gmail.com>
2020-07-11 13:22:47 +0300

    dependencies update

---
+++ auth_system/backend/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\software development\django-react-apps\auth_system\backend\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
