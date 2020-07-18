587a4fcfd9464d5ef0e1c8c2ebec370d63653c07
MartinMwiti <martinmwiti007@gmail.com>
2020-07-18 12:41:16 +0300

    update

--- real_estate/frontend/src/components/ListingForm.js
+++ real_estate/frontend/src/components/ListingForm.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios"
+import Loader from "react-loader-spinner"
+import PropTypes from "prop-types"
+
 
 const ListingForm =() => {
+    
+
+
     return (
         <div>
             
